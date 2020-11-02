477aad8f8aaa4395646c9f4c017043d2dcbf7a1f
bread01 <bmorin8@wgu.edu>
2020-11-02 14:36:44 -0500

    very simple calculator done

--- Section-1/Simple-Calculator/script.js
+++ Section-1/Simple-Calculator/script.js
@@ -1,35 +1,4 @@
-var database = [
-    {
-        username: "andrei",
-        password: "supersecret",
-    }
-];
-
-var newsfeed = [
-    {
-        username: "bobby",
-        timeline: "so tired from learning"
-    },
-    {
-        username: "sally",
-        timeline: "javascript rocks"
-    }
-];
-
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
-
-function signIn(user, pass) {
-    if (user === database[0].username && pass === 
-        database[0].password) {
-            console.log(newsfeed);
-        } else {
-            alert("Sorry, wrong username and password");
-        }
-
-}
-
-signIn(userNamePrompt, passwordPrompt);
-
-let sum = (4 + 5);
-console.log(sum);
+let firstNumber = prompt("What is your first number?");
+let secondNumber = prompt("What is your second number?");
+let sum = Number(firstNumber) + Number(secondNumber);
+alert(sum);
