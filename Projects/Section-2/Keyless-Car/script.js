5a1a7b3dd6bf3fb2d99be79c7eb3f667e598774f
bread01 <bmorin8@wgu.edu>
2020-11-04 14:30:21 -0500

    project keyless car

--- Projects/Section-2/Keyless-Car/script.js
+++ Projects/Section-2/Keyless-Car/script.js
@@ -1,5 +1,25 @@
-function a() {
-    let y = 2;
-    return(y);
-  }
-  console.log(a(4*5));
+//using prompt() and alert(), ask a user for their age.
+function askAge() {
+    let age = prompt("What is your age");
+    if (age < 18) {
+        alert("Sorry, you are too young to drive this car. Powering off");
+    } else if (age === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    } else if (age > 18) {
+        alert("Powering On. Enjoy the ride!");
+    }
+    return age;
+}
+
+askAge();
+
+// IF they say they are below 18, respond with:
+// "Sorry, you are too young to drive this car. Powering off"
+
+
+// IF they say they are 18, respond with:
+// "Congratulations on your first year of driving. Enjoy the ride!"
+
+
+// IF they say they are over 18, respond with:
+// "Powering On. Enjoy the ride!"
